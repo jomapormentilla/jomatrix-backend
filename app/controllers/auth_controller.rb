57f7c158da7fc7fdc4837105479782fcf714ebe9
jomapormentilla <jomapormentilla@gmail.com>
2021-03-15 17:07:35 -0400

    Add Authentication route. Send JWT for authorized users.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,14 +1,14 @@
 class AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
-    
+
     def create
         user = User.find_by(email: user_login_params[:email])
 
         if user && user.authenticate(user_login_params[:password])
             token = encode_token({ user_id: user.id })
-            render json: { user: UserSerializer.new(user), jwt: token }, status: :accepted
+            render json: { data: UserSerializer.new(user), jwt: token }, status: :accepted
         else
-            render json: { message: 'Invalid Credentials.' }, status: :unauthorized
+            render json: { error: 'Invalid Credentials.' }, status: :unauthorized
         end
     end
 
