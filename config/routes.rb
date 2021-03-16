bb618f40f21198e768bde37752b9093ac136b2b4
jomapormentilla <jomapormentilla@gmail.com>
2021-03-16 01:25:54 -0400

    Enable Profile route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users
 
   post '/login' => "auth#create"
+  get '/profile' => "users#profile"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
