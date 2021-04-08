f474e018fa0c135b373e177d96158b89cb0236a2
jomapormentilla <jomapormentilla@gmail.com>
2021-04-08 18:42:38 -0400

    Update cors to allow new Netlify site.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,4 +13,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+  allow do
+    origins 'https://youthful-jepsen-16643f.netlify.app'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 end
